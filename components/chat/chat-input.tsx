5ec6f1d156a2521af5eeca1cd9023b36d567e5a7
Thomas Tay <thomastayac@gmail.com>
2024-04-08 22:38:02 -0700

    Revert "Don't show file picker, and don't react to slash commands"
    This reverts commit 4398b35470aae060e1f0352bf2aec8463f69741d.

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -216,12 +216,32 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
           <ChatCommandInput />
         </div>
 
+        <>
+          <IconCirclePlus
+            className="absolute bottom-[12px] left-3 cursor-pointer p-1 hover:opacity-50"
+            size={32}
+            onClick={() => fileInputRef.current?.click()}
+          />
+
+          {/* Hidden input to select files from device */}
+          <Input
+            ref={fileInputRef}
+            className="hidden"
+            type="file"
+            onChange={e => {
+              if (!e.target.files) return
+              handleSelectDeviceFile(e.target.files[0])
+            }}
+            accept={filesToAccept}
+          />
+        </>
+
         <TextareaAutosize
           textareaRef={chatInputRef}
           className="ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring text-md flex w-full resize-none rounded-md border-none bg-transparent px-14 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           placeholder={t(
             // `Ask anything. Type "@" for assistants, "/" for prompts, "#" for files, and "!" for tools.`
-            `Ask anything. Type @  #  !`
+            `Ask anything. Type @  /  #  !`
           )}
           onValueChange={handleInputChange}
           value={userInput}
